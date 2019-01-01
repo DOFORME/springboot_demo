0720b0d13b6247f428edfce39869ebdc280acd8a
lc <15501673636@163.com>
2019-01-01 22:43:08 +0800

    update

---
+++ src/main/java/com/lc/demo/se/lambda/MyNum.java
@@ -0,0 +1,22 @@
+package com.lc.demo.se.lambda;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MyNum implements Comparable<MyNum> {
+
+    private int num;
+
+    @Override
+    public int compareTo(MyNum o) {
+        if (o.num == num) {
+            return 0;
+        } else if(o.num > num) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ src/main/java/com/lc/demo/se/lambda/MyNumComparator.java
@@ -0,0 +1,20 @@
+package com.lc.demo.se.lambda;
+
+import java.util.Comparator;
+
+public class MyNumComparator implements Comparator<MyNum> {
+
+    @Override
+    public int compare(MyNum o1, MyNum o2) {
+        if (o1 == null || o2 == null) {
+            throw new RuntimeException("can't compare null");
+        }
+        if (o1.getNum() < o2.getNum()) {
+            return 1;
+        } else if(o1.getNum() == o2.getNum()) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
